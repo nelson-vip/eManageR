b622f6aec13ea63f600cd56d9901f09369163d7c
Nelson <nelson211145@gmail.com>
2012-10-02 10:24:57 +0800

    init

---
+++ eManager.Core/Exam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eManager.Core
+{
+    public class Exam
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual int Level { get; set; }
+        public virtual int Year { get; set; }
+        public virtual int Location { get; set; }
+        public virtual int County { get; set; }
+        public virtual ICollection<Question> Questions { get; set; }
+    }
+}
---
+++ eManager.Core/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eManager.Core
+{
+    public class Question
+    {
+        public virtual int Id { get; set; }
+        public virtual int Index { get; set; }
+        public virtual string Content { get; set; }
+    }
+}
